177e439c724348a739dc7c3e389e797321ef8205
Prateek Agarwal <prateekagr98@gmail.com>
2017-05-13 18:49:51 +0530

    Setup express server

---
+++ bin/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('babel-register');
+
+let express 			= require('express');
+let morgan 				= require('morgan');
+let path  				= require('path');
+
+let server = express();
+
+let port = 5000;
+
+server.use(morgan('dev'));
+
+server.use('/assets', express.static('build'));
+
+server.use('/', function(req, res, next){
+	res.sendFile(path.normalize(__dirname + '/../app/index.html'));
+});
+
+server.listen(port, function(){
+	console.log('\nListening on port ' + port);
+});
